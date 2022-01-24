b9c8565a156ccc34e4ed631b1efe5cb9f76ca47d
developer <87354530+developer12s@users.noreply.github.com>
2022-01-24 13:14:12 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,22 +1,9 @@
 provider "aws"{
   region=var.region
-  }
-locals{
-serverconfig = [
-  for srv in var.ami : [
-    for i in 1 : 
-      {
-        ami = srv.ami
-    }
-  ]
-}
-  locals {
-  instances = flatten(local.serverconfig)
 }
  
 
 resource "aws_instance" "web" {
-  for_each = {for server in local.instances}
   ami           = each.value.ami
   instance_type = var.type
   tags={
